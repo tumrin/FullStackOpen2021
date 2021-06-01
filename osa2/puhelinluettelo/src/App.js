263e7262d24e341b03a3ca426b3e2f9e070aa3af
Tuomas Rinne <tuomas.rin@hotmail.com>
2021-06-01 19:30:37 +0300

    tehtävä 2.11

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
 
 const Person = ({person}) =>{
   return(
@@ -28,17 +29,20 @@ const PersonForm = ({addPerson, newName, nameHandler, newNumber, numberHandler})
   )
 }
 
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newFilter, setNewFilter] = useState("");
   const [shownPersons, setShownPersons] = useState(persons);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+      setShownPersons(response.data)})
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault();
