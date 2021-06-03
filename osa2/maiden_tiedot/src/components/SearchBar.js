e8700f39958d7a723785c7ebb515d763652d71e0
Tuomas Rinne <tuomas.rin@hotmail.com>
2021-06-03 19:44:44 +0300

    tehtävä 2.13

--- osa2/maiden_tiedot/src/components/SearchBar.js
+++ osa2/maiden_tiedot/src/components/SearchBar.js
@@ -12,8 +12,16 @@ const SearchBar = () => {
     },[country])
     return (
         <div className="searchbar">
+            Find countries:
             <input type="text" onChange={(event)=>setCountry(event.target.value)}/>
-            {showCountry.length>10 ? <p>Too many matches</p> : showCountry.length === 1 ? <Country country={showCountry[0]}/> : showCountry.map(entry => <p>{entry.name}</p>)}
+            {showCountry.length>10 ? <p>Too many matches</p> : showCountry.length === 1 ? <Country country={showCountry[0]}/> : showCountry.map(entry => {
+            return(
+            <div>
+            <p style={{display:'inline'}}>{entry.name}</p>
+            <button type="button" onClick={()=> setShowCountry(showCountry.filter(country => country.name===entry.name))} style={{display: 'inline'}}>show</button>
+            </div>
+            )
+            })}
         </div>
     )
 }
