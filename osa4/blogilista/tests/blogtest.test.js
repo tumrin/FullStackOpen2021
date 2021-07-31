fd92322a96161c99fd42ef4dc6b592a9fb89ac79
Tuomas Rinne <tuomas.rin@hotmail.com>
2021-07-31 18:38:34 +0300

    Tehtävä 4.4

--- osa4/blogilista/tests/blogtest.test.js
+++ osa4/blogilista/tests/blogtest.test.js
@@ -6,3 +6,45 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('Total likes', () => {
+
+    test('Empty list', ()=>{
+        const blogs = []
+        expect(listHelper.totalLikes(blogs)).toBe(0)
+    })
+
+    test('List has 1 blog', ()=>{
+        const blog = {
+            title: "Testi",
+            author: "Testi henkilö",
+            url: "Unknown",
+            likes: 1
+        }
+       const  blogs = [blog]
+        expect(listHelper.totalLikes(blogs)).toBe(1)
+    })
+
+    test('Has multiple blogs', ()=>{
+        const blog = {
+            title: "Testi",
+            author: "Testi henkilö",
+            url: "Unknown",
+            likes: 1
+        }
+        const blog1 = {
+            title: "Testi1",
+            author: "Testi henkilö1",
+            url: "Unknown",
+            likes: 2
+        }
+        const blog2 = {
+            title: "Testi2",
+            author: "Testi henkilö2",
+            url: "Unknown",
+            likes: 3
+        }
+       const  blogs = [blog, blog1, blog2]
+        expect(listHelper.totalLikes(blogs)).toBe(6)
+    })
+})
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -2,6 +2,10 @@ const dummy = (blogs) => {
     return 1
   }
   
-  module.exports = {
-    dummy
+const totalLikes = (blogs) => {
+    return blogs.reduce((sum, blog) => sum + blog.likes, 0) || 0
+}
+module.exports = {
+    dummy,
+    totalLikes,
   }
