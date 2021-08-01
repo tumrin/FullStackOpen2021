5ceb676ce40efd9c6ac1a29046f54e7a24afc245
Tuomas Rinne <tuomas.rin@hotmail.com>
2021-08-01 10:47:24 +0300

    Tehtävä 4.5

--- osa4/blogilista/tests/blogtest.test.js
+++ osa4/blogilista/tests/blogtest.test.js
@@ -48,3 +48,45 @@ describe('Total likes', () => {
         expect(listHelper.totalLikes(blogs)).toBe(6)
     })
 })
+
+describe('Favorite blog', () => {
+
+    test('Empty list', () => {
+        const blogs = []
+        expect(listHelper.favoriteBlog(blogs)).toEqual(null)
+    })
+
+    test('One blog', () => {
+        const blog = {
+            title: "Testi",
+            author: "Testi henkilö",
+            url: "Unknown",
+            likes: 1
+        }
+        const blogs = [blog]
+        expect(listHelper.favoriteBlog(blogs)).toEqual(blog)
+    })
+
+    test('Has multiple blogs', ()=>{
+        const blog = {
+            title: "Testi",
+            author: "Testi henkilö",
+            url: "Unknown",
+            likes: 1
+        }
+        const blog1 = {
+            title: "Testi1",
+            author: "Testi henkilö1",
+            url: "Unknown",
+            likes: 2
+        }
+        const blog2 = {
+            title: "Testi2",
+            author: "Testi henkilö2",
+            url: "Unknown",
+            likes: 3
+        }
+       const  blogs = [blog, blog1, blog2]
+        expect(listHelper.favoriteBlog(blogs)).toEqual(blog2)
+    })
+})
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -5,7 +5,21 @@ const dummy = (blogs) => {
 const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0) || 0
 }
+
+const favoriteBlog = (blogs) => {
+  let favBlog = blogs[0]
+
+  blogs.forEach(element => {
+    if(element.likes > favBlog.likes){
+      favBlog = element
+    }
+  });
+
+  return favBlog || null
+}
+
 module.exports = {
     dummy,
     totalLikes,
+    favoriteBlog
   }
