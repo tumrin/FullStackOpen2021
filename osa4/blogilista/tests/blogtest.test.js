b64defca37a0cf93c67dc96835df0d554e6952e2
Tuomas Rinne <tuomas.rin@hotmail.com>
2021-08-01 13:07:36 +0300

    Testien siistimistä Tehtäviin 4.3-4.7

--- osa4/blogilista/tests/blogtest.test.js
+++ osa4/blogilista/tests/blogtest.test.js
@@ -1,5 +1,33 @@
 const listHelper = require('../utils/list_helper')
 
+const noBlogs = []
+const oneBlog = [{
+    title: "Testi",
+    author: "Testi henkilö",
+    url: "Unknown",
+    likes: 1
+}]
+const multipleBlogs = [
+    {
+        title: "Testi",
+        author: "Testi henkilö",
+        url: "Unknown",
+        likes: 1
+    },
+    {
+        title: "Testi1",
+        author: "Testi henkilö1",
+        url: "Unknown",
+        likes: 2
+    },
+    {
+        title: "Testi2",
+        author: "Testi henkilö2",
+        url: "Unknown",
+        likes: 3
+    }
+]
+
 test('dummy returns one', () => {
   const blogs = []
 
@@ -7,239 +35,179 @@ test('dummy returns one', () => {
   expect(result).toBe(1)
 })
 
+////TOTAL LIKES//////////////////////////////////////
 describe('Total likes', () => {
 
     test('Empty list', ()=>{
-        const blogs = []
-        expect(listHelper.totalLikes(blogs)).toBe(0)
+        expect(listHelper.totalLikes(noBlogs)).toBe(0)
     })
 
     test('List has 1 blog', ()=>{
-        const blog = {
-            title: "Testi",
-            author: "Testi henkilö",
-            url: "Unknown",
-            likes: 1
-        }
-       const  blogs = [blog]
-        expect(listHelper.totalLikes(blogs)).toBe(1)
+        expect(listHelper.totalLikes(oneBlog)).toBe(1)
     })
 
     test('Has multiple blogs', ()=>{
-        const blog = {
-            title: "Testi",
-            author: "Testi henkilö",
-            url: "Unknown",
-            likes: 1
-        }
-        const blog1 = {
-            title: "Testi1",
-            author: "Testi henkilö1",
-            url: "Unknown",
-            likes: 2
-        }
-        const blog2 = {
-            title: "Testi2",
-            author: "Testi henkilö2",
-            url: "Unknown",
-            likes: 3
-        }
-       const  blogs = [blog, blog1, blog2]
-        expect(listHelper.totalLikes(blogs)).toBe(6)
+        expect(listHelper.totalLikes(multipleBlogs)).toBe(6)
     })
 })
+////TOTAL LIKES//////////////////////////////////////
 
+////FAVORITE BLOG//////////////////////////////////////
 describe('Favorite blog', () => {
 
     test('Empty list', () => {
-        const blogs = []
-        expect(listHelper.favoriteBlog(blogs)).toEqual(null)
+        expect(listHelper.favoriteBlog(noBlogs)).toEqual(null)
     })
 
     test('One blog', () => {
-        const blog = {
+        expect(listHelper.favoriteBlog(oneBlog)).toEqual({
             title: "Testi",
             author: "Testi henkilö",
             url: "Unknown",
             likes: 1
-        }
-        const blogs = [blog]
-        expect(listHelper.favoriteBlog(blogs)).toEqual(blog)
+        })
     })
 
     test('Has multiple blogs', ()=>{
-        const blog = {
+        expect(listHelper.favoriteBlog(multipleBlogs)).toEqual(        {
+            title: "Testi2",
+            author: "Testi henkilö2",
+            url: "Unknown",
+            likes: 3
+        })
+    })
+})
+////FAVORITE BLOG//////////////////////////////////////
+
+////MOST BLOGS//////////////////////////////////////
+describe('Most blogs', () => {
+    const multipleBlogs = [
+        {
             title: "Testi",
-            author: "Testi henkilö",
+            author: "Testi henkilö1",
             url: "Unknown",
             likes: 1
-        }
-        const blog1 = {
+        },
+        {
             title: "Testi1",
             author: "Testi henkilö1",
             url: "Unknown",
             likes: 2
-        }
-        const blog2 = {
+        },
+        {
             title: "Testi2",
             author: "Testi henkilö2",
             url: "Unknown",
             likes: 3
         }
-       const  blogs = [blog, blog1, blog2]
-        expect(listHelper.favoriteBlog(blogs)).toEqual(blog2)
-    })
-})
-
-describe('Most blogs', () => {
-
-    test('Empty list', () => {
-        const blogs = []
-        expect(listHelper.mostBlogs(blogs)).toEqual(null)
-    })
-
-    test('One blog', () => {
-        const blog = {
+    ]
+    const multipleMostBlogs = [
+        {
             title: "Testi",
-            author: "Testi henkilö",
+            author: "Testi henkilö1",
             url: "Unknown",
             likes: 1
-        }
-        const blogs = [blog]
-        expect(listHelper.mostBlogs(blogs)).toEqual({author:'Testi henkilö', blogs: 1})
-    })
-
-    test('Has multiple blogs', ()=>{
-        const blog = {
-            title: "Testi",
-            author: "Testi henkilö",
-            url: "Unknown",
-            likes: 1
-        }
-        const blog1 = {
+        },
+        {
             title: "Testi1",
             author: "Testi henkilö1",
             url: "Unknown",
             likes: 2
-        }
-        const blog2 = {
+        },
+        {
             title: "Testi2",
-            author: "Testi henkilö1",
+            author: "Testi henkilö2",
             url: "Unknown",
             likes: 3
-        }
-        const blog3 = {
+        },
+        {
             title: "Testi3",
-            author: "Testi henkilö3",
+            author: "Testi henkilö2",
             url: "Unknown",
             likes: 3
         }
-       const  blogs = [blog, blog1, blog2, blog3]
-        expect(listHelper.mostBlogs(blogs)).toEqual({author:'Testi henkilö1', blogs: 2})
+    ]
+
+    test('Empty list', () => {
+        expect(listHelper.mostBlogs(noBlogs)).toEqual(null)
+    })
+
+    test('One blog', () => {
+
+        expect(listHelper.mostBlogs(oneBlog)).toEqual({author:'Testi henkilö', blogs: 1})
+    })
+
+    test('Has multiple blogs', ()=>{
+        expect(listHelper.mostBlogs(multipleBlogs)).toEqual({author:'Testi henkilö1', blogs: 2})
     })
 
     test('Multiple most blogs', () => {
-        const blog = {
+        expect(listHelper.mostBlogs(multipleMostBlogs)).toEqual({author:'Testi henkilö1', blogs: 2})
+    })
+})
+////MOST BLOGS//////////////////////////////////////
+
+////MOST LIKES//////////////////////////////////////
+describe('Most Likes', () => {
+    const multipleBlogs = [
+        {
             title: "Testi",
-            author: "Testi henkilö",
+            author: "Testi henkilö1",
             url: "Unknown",
             likes: 1
-        }
-        const blog1 = {
+        },
+        {
             title: "Testi1",
             author: "Testi henkilö1",
             url: "Unknown",
             likes: 2
-        }
-        const blog2 = {
+        },
+        {
             title: "Testi2",
+            author: "Testi henkilö2",
+            url: "Unknown",
+            likes: 3
+        }
+    ]
+    const multipleMostLikes = [
+        {
+            title: "Testi",
             author: "Testi henkilö1",
             url: "Unknown",
             likes: 3
-        }
-        const blog3 = {
+        },
+        {
+            title: "Testi1",
+            author: "Testi henkilö1",
+            url: "Unknown",
+            likes: 3
+        },
+        {
+            title: "Testi2",
+            author: "Testi henkilö2",
+            url: "Unknown",
+            likes: 3
+        },
+        {
             title: "Testi3",
-            author: "Testi henkilö",
+            author: "Testi henkilö2",
             url: "Unknown",
             likes: 3
         }
-       const  blogs = [blog, blog1, blog2, blog3]
-        expect(listHelper.mostBlogs(blogs)).toEqual({author:'Testi henkilö', blogs: 2})
-    })
-})
-
-describe('Most Likes', () => {
-
+    ]
     test('Empty list', () => {
-        const blogs = []
-        expect(listHelper.mostLikes(blogs)).toEqual(null)
+        expect(listHelper.mostLikes(noBlogs)).toEqual(null)
     })
 
     test('One blog', () => {
-        const blog = {
-            title: "Testi",
-            author: "Testi henkilö",
-            url: "Unknown",
-            likes: 1
-        }
-        const blogs = [blog]
-        expect(listHelper.mostLikes(blogs)).toEqual({author:'Testi henkilö', likes: 1})
+        expect(listHelper.mostLikes(oneBlog)).toEqual({author:'Testi henkilö', likes: 1})
     })
 
     test('Has multiple blogs', ()=>{
-        const blog = {
-            title: "Testi",
-            author: "Testi henkilö",
-            url: "Unknown",
-            likes: 1
-        }
-        const blog1 = {
-            title: "Testi1",
-            author: "Testi henkilö1",
-            url: "Unknown",
-            likes: 2
-        }
-        const blog2 = {
-            title: "Testi2",
-            author: "Testi henkilö1",
-            url: "Unknown",
-            likes: 3
-        }
-        const blog3 = {
-            title: "Testi3",
-            author: "Testi henkilö3",
-            url: "Unknown",
-            likes: 3
-        }
-       const  blogs = [blog, blog1, blog2, blog3]
-        expect(listHelper.mostLikes(blogs)).toEqual({author:'Testi henkilö1', likes: 5})
+        expect(listHelper.mostLikes(multipleBlogs)).toEqual({author:'Testi henkilö1', likes: 3})
     })
     test('Multiple most Likes', () => {
-        const blog = {
-            title: "Testi",
-            author: "Testi henkilö",
-            url: "Unknown",
-            likes: 2
-        }
-        const blog1 = {
-            title: "Testi1",
-            author: "Testi henkilö1",
-            url: "Unknown",
-            likes: 2
-        }
-        const blog2 = {
-            title: "Testi2",
-            author: "Testi henkilö1",
-            url: "Unknown",
-            likes: 3
-        }
-        const blog3 = {
-            title: "Testi3",
-            author: "Testi henkilö",
-            url: "Unknown",
-            likes: 3
-        }
-       const  blogs = [blog, blog1, blog2, blog3]
-        expect(listHelper.mostLikes(blogs)).toEqual({author:'Testi henkilö', likes: 5})
+        expect(listHelper.mostLikes(multipleMostLikes)).toEqual({author:'Testi henkilö1', likes: 6})
     })
 })
+////MOST LIKES//////////////////////////////////////
