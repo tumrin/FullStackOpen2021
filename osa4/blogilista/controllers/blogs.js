514287d796c221370f73431d57809c85d052b39a
Tuomas Rinne <tuomas.rin@hotmail.com>
2021-08-29 11:19:42 +0300

    Tehtävä 4.17

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,9 +1,10 @@
 const blogsRouter = require('express').Router()
 const { response } = require('express')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', {username:1, name:1})
   response.json(blogs.map((blog) => blog.toJSON()))
 })
 
@@ -14,9 +15,14 @@ blogsRouter.post('/', async (request, response) => {
   if (!request.body.url || !request.body.title) {
     response.status(400).end()
   } else {
+    const user = await User.find({})
+
     const blog = new Blog(request.body)
+    blog.user = user[0]._id
     const result = await blog.save()
-    response.status(204).json(result)
+    user[0].blogs = user[0].blogs.concat(result._id)
+    await user[0].save()
+    response.status(204).json(result.toJSON)
   }
 })
 blogsRouter.delete('/:id', async (request, response) => {
--- osa4/blogilista/controllers/users.js
+++ osa4/blogilista/controllers/users.js
@@ -2,6 +2,13 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+usersRouter.get('/', async (Request, response) => {
+  const users = await User
+  .find({}).populate('blogs', { url: 1, title: 1 , author: 1})
+
+response.json(users.map(u => u.toJSON()))
+})
+
 usersRouter.post('/', async (request, response, next) => {
   const body = request.body
 
@@ -12,6 +19,7 @@ usersRouter.post('/', async (request, response, next) => {
   }
   const passwordHash = await bcrypt.hash(body.password, saltRounds)
     const user = new User({
+      blogs: [],
       username: body.username,
       name: body.name,
       passwordHash,
