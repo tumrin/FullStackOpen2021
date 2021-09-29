da474df886d00476a238e1daf6711dc94874bfbb
Tuomas Rinne <tuomas.rin@hotmail.com>
2021-09-30 00:23:40 +0300

    Tehtävä 5.7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,27 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-    <div>
-        {blog.title} {blog.author}
-    </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+    const [detail, showDetail] = useState(false)
+    if (detail) {
+        return (
+            <div style={{ borderStyle: 'solid', marginBottom: 10, padding: 2 }}>
+                {blog.title} {blog.author}{' '}
+                <button onClick={() => showDetail(false)}>hide</button>
+                <br />
+                {blog.url}
+                <br />
+                {blog.likes} <button>like</button>
+                <br />
+                {blog.user.name}
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                {blog.title} {blog.author}
+                <button onClick={() => showDetail(true)}>show</button>
+            </div>
+        )
+    }
+}
 
 export default Blog
