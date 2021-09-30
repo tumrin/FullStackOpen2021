beb4048550c29f424ae2acf2d736795493e12477
Tuomas Rinne <tuomas.rin@hotmail.com>
2021-09-30 20:35:43 +0300

    Tehtävä 5.14

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 test('Does not render url and likes', () => {
@@ -17,4 +17,26 @@ test('Does not render url and likes', () => {
     'Testauksen alkeet' && 'Testi Henkilö'
   )
   expect(component.container).not.toHaveTextContent('testaus.test' && 2)
+})
+
+test('Renders url and likes', () => {
+  const user = {
+    name: 'Toinen Testaaja',
+    username: 'tt',
+  }
+  const blog = {
+    title: 'Tastauksen alkeet',
+    author: 'Testi Henkilö',
+    url: 'testaus.test',
+    likes: 2,
+    user: user,
+  }
+
+  const component = render(<Blog blog={blog} user={user} />)
+  const button = component.container.querySelector('button')
+  fireEvent.click(button)
+
+  expect(component.container).toHaveTextContent(
+    'Testauksen alkeet' && 'Testi Henkilö' && 'testaus.test' && 2
+  )
 })
